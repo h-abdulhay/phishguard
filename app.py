1a690bee3b3d59894fb950c028453e883a2ddf87
Abdulhay <abdulhayhakimov07@gmail.com>
2026-04-29 03:40:11 +0500

    fix host and port

--- app.py
+++ app.py
@@ -333,4 +333,4 @@ def clear_history():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
